d18f12195ce052d74c4d9e9d91e03912031c53ed
roman-sereda <romansereda98@gmail.com>
2017-05-29 14:34:54 +0300

    Add methods to detect captured zones

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -38,10 +38,97 @@ class FieldsController < ApplicationController
   def find_captured_zones
 
     if there_is_nearest_points?
-      p 'yes'
-    else
-      p 'no'
+      search_captured_zone
     end
+
+  end
+
+  def search_captured_zone
+
+    p '!'
+
+    4.times do |i|
+      if(@field.points[@x + @steps[:x][i]][@y + @steps[:y][i]] == "0")
+        last_turn = 0
+        has_end = true
+        current_position = [ @x + @steps[:x][i] , @y + @steps[:y][i] ]
+        checked_positions = []
+
+        checked_positions.push( current_position )
+          while(true)
+            last_turn += 1
+
+            4.times do |j|
+              next_position = [ current_position[0] + @steps[:x][j], current_position[1] + @steps[:y][j] ]
+
+              if(@field.points[next_position[0]][next_position[1]] == "0" && !checked_positions.include?(next_position) )
+                checked_positions.push(next_position)
+              end
+            end
+
+            break if checked_positions.length == last_turn
+
+            current_position = checked_positions[last_turn]
+
+            if (current_position[0] > 18 || current_position[0] < 0 || current_position[1] > 18 || current_position[1] < 0)
+    					has_end = false;
+    					break;
+    				end
+          end
+
+          if( has_end )
+            create_capture_zone(checked_positions)
+          end
+      end
+    end
+
+  end
+
+  def create_capture_zone checked_positions
+
+    p checked_positions
+
+    capture_points = []
+    capture_zone = []
+
+    checked_positions.each do | pos |
+      4.times do |i|
+        p pos[0]
+        next_checked = [ pos[0] + @steps[:x][i], pos[1] + @steps[:y][i] ]
+        p next_checked
+
+        if( @field.points[next_checked[0]][next_checked[1]] == "1" && !capture_points.include?( next_checked ))
+          capture_points.push( next_checked )
+        end
+      end
+    end
+
+    capture_points.each do |pos|
+      p pos
+    end
+
+    temp_point = capture_points[0]
+
+    p capture_points.length
+
+    capture_points.length.times do |i|
+      p i
+      capture_points.length.times do |j|
+        4.times do |k|
+          temp_zone = [ temp_point[0] + @steps[:x][k], temp_point[1] + @steps[:y][k] ]
+
+          p "! #{temp_zone}"
+          p "# #{capture_points[j]}"
+
+          if(temp_zone == capture_points[j] && !capture_zone.include?( temp_zone ))
+            capture_zone.push( temp_point )
+            temp_point = temp_zone
+          end
+        end
+      end
+    end
+
+    p capture_zone
 
   end
 
