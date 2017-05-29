6e859e8e4fe753f9dcc820f13acc044d2fcd7a17
roman-sereda <romansereda98@gmail.com>
2017-05-29 11:44:13 +0300

    Add function to check for nearest points

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -22,12 +22,38 @@ class FieldsController < ApplicationController
   end
 
   def new_point
-    field = Field.find(params[:id])
+    @field = Field.find(params[:id])
+    @x = params[:x].to_i
+    @y = params[:y].to_i
+    @steps = { x: [0,-1,1,0], y: [-1,0,0,1] }
 
-    temp_points = field.points
-    temp_points[params[:x].to_i][params[:y].to_i] = 1
+    temp_points = @field.points
+    temp_points[@x][@y] = 1
 
-    field.update_attributes(points: temp_points)
+    @field.update_attributes(points: temp_points)
+
+    find_captured_zones
+  end
+
+  def find_captured_zones
+
+    if there_is_nearest_points?
+      p 'yes'
+    else
+      p 'no'
+    end
+
+  end
+
+  def there_is_nearest_points?
+
+    count = 0
+
+    4.times.each do |i|
+      count+=1 if @field.points[@x + @steps[:x][i]][@y + @steps[:y][i]] != "0"
+    end
+
+    return count >= 2 ? true : false
   end
 
   def show
